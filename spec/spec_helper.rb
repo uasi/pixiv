fe9387c8cc35d829095534194b1381a5ee9b5c87
Tomoki Aonuma <uasi@uasi.jp>
2012-12-23 20:52:21 +0900

    Add spec_helper.rb

---
+++ spec/spec_helper.rb
@@ -0,0 +1,22 @@
+require 'pixiv'
+require 'mechanize'
+require 'webmock/rspec'
+
+# Disable "should" syntax.
+RSpec.configure do |config|
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
+end
+
+def fixture_path
+  File.expand_path('../fixtures', __FILE__)
+end
+
+def fixture(filename)
+  open(File.join(fixture_path, filename))
+end
+
+def fixture_as_doc(filename)
+  Nokogiri::HTML::Document.parse(fixture(filename))
+end
