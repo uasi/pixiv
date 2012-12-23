54d46003aa47ce2f1ee57c07d6d5f0a67aa1501c
Tomoki Aonuma <uasi@uasi.jp>
2012-12-23 16:02:12 +0900

    Rename #illust_hash to #illust_hashes

--- lib/pixiv/bookmark_list.rb
+++ lib/pixiv/bookmark_list.rb
@@ -10,11 +10,11 @@ module Pixiv
     lazy_attr_reader(:last?) { at!('li.pages-current').next_element.inner_text.to_i == 0 }
     lazy_attr_reader(:member_id) { doc.body.match(/pixiv\.context\.userId = '(\d+)'/).to_a[1].to_i }
     lazy_attr_reader(:illust_ids) { search!('li[id^="li_"] a[href^="member_illust.php?mode=medium"]').map {|n| n.attr('href').match(/illust_id=(\d+)$/).to_a[1].to_i } }
-    lazy_attr_reader(:illust_hash) {
+    lazy_attr_reader(:illust_hashes) {
       search!('li[id^="li_"]').map {|node| illust_hash_from_bookmark_item(node) }.compact
     }
 
-    alias page_hashes illust_hash
+    alias page_hashes illust_hashes
 
     def url; self.class.url(member_id, page_num) end
     def first?; page_num == 1 end
