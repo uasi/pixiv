08a9c96b5bbd2c0a24055dd68d31e86fb4464f37
Tomoki Aonuma <uasi@uasi.jp>
2012-12-25 06:24:51 +0900

    Add member_spec.rb and fixtures for it

---
+++ spec/pixiv/member_spec.rb
@@ -0,0 +1,32 @@
+require 'spec_helper'
+
+describe Pixiv::Member do
+  describe '.new' do
+    subject { Pixiv::Member.new(fixture_as_doc('member_6.html')) }
+    its(:member_id) { should == 6 }
+    its(:pixiv_id) { should == 'sayoko' }
+    its(:name) { should == 'Sayoko' }
+  end
+
+  describe '.new', 'given attrs cover attrs extracted from doc' do
+    before do
+      @doc = fixture_as_doc('member_6.html')
+      @attrs = {
+        member_id: 13,
+        pixiv_id: 'duke',
+        name: 'Duke',
+      }
+    end
+    subject { Pixiv::Member.new(@doc, @attrs) }
+    its(:member_id) { should == 13 }
+    its(:pixiv_id) { should == 'duke' }
+    its(:name) { should == 'Duke' }
+  end
+
+  describe '.new', 'attr raises NodeNotFound if doc is wrong' do
+    subject { Pixiv::Member.new(fixture_as_doc('empty.html')) }
+    it { expect { subject.member_id }.to raise_error(Pixiv::Error::NodeNotFound) }
+    it { expect { subject.pixiv_id }.to raise_error(Pixiv::Error::NodeNotFound) }
+    it { expect { subject.name}.to raise_error(Pixiv::Error::NodeNotFound) }
+  end
+end
