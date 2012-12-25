c367d320977b76a82b8010b68ba7f5aef7d86900
Tomoki Aonuma <uasi@uasi.jp>
2012-12-25 23:52:16 +0900

    Add Pixiv.client and deprecate Pixiv.new

--- lib/pixiv.rb
+++ lib/pixiv.rb
@@ -10,8 +10,15 @@ require 'pixiv/bookmark_list'
 module Pixiv
   ROOT_URL = 'http://www.pixiv.net'
 
+  # @deprecated Use {.client} instead. Will be removed in 0.1.0.
   # Delegates to {Pixiv::Client#initialize}
   def self.new(*args, &block)
+    Pixiv::Client.new(*args, &block)
+  end
+
+  # See {Pixiv::Client#initialize}
+  # @return [Pixiv::Client]
+  def self.client(*args, &block)
     Pixiv::Client.new(*args, &block)
   end
 end
