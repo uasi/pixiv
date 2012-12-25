ee937be9267878d7a13b203c7bf839b93cc62725
Tomoki Aonuma <uasi@uasi.jp>
2012-12-26 03:51:29 +0900

    Autoload modules

--- lib/pixiv.rb
+++ lib/pixiv.rb
@@ -1,14 +1,16 @@
 require 'mechanize'
-require 'pixiv/error'
-require 'pixiv/client'
-require 'pixiv/page'
-require 'pixiv/illust'
-require 'pixiv/member'
-require 'pixiv/page_collection'
-require 'pixiv/bookmark_list'
-require 'pixiv/illust_list'
 
 module Pixiv
+  autoload :PrivateBookmarkList, 'pixiv/bookmark_list'
+  autoload :BookmarkList,        'pixiv/bookmark_list'
+  autoload :Client,              'pixiv/client'
+  autoload :Error,               'pixiv/error'
+  autoload :Illust,              'pixiv/illust'
+  autoload :IllustList,          'pixiv/illust_list'
+  autoload :Member,              'pixiv/member'
+  autoload :Page,                'pixiv/page'
+  autoload :PageCollection,      'pixiv/page_collection'
+
   ROOT_URL = 'http://www.pixiv.net'
 
   # @deprecated Use {.client} instead. Will be removed in 0.1.0.
