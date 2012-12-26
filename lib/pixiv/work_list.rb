cd5e0d52c7278a28ea49fdc231fd47cfc874f861
Tomoki Aonuma <uasi@uasi.jp>
2012-12-26 21:33:10 +0900

    WorkList gets overhauled

--- lib/pixiv/work_list.rb
+++ lib/pixiv/work_list.rb
@@ -1,28 +1,56 @@
 module Pixiv
-  # FIXME: IllustList should be parent of BookmarkList
-  class WorkList < BookmarkList
-    include PageCollection
-
-    def self.url(member_id, page_num = 1)
-      "#{ROOT_URL}/member_illust.php?id=#{member_id}&p=#{page_num}"
+  class WorkList < IllustList
+    def self.url(member_id, page = 1)
+      "#{ROOT_URL}/member_illust.php?id=#{member_id}&p=#{page}"
     end
 
     # @return [Integer]
-    lazy_attr_reader(:count) {
-      at!('.layout-cell .count-badge').inner_text.match(/\d+/).to_s.to_i
+    lazy_attr_reader(:page) {
+      at!('li.pages-current').inner_text.to_i
     }
-    # @return [Array<Integer>]
-    lazy_attr_reader(:illust_ids) {
-      search!('.display_works li a').map {|n| n['href'].match(/illust_id=(\d+)$/).to_a[1].to_i }
+    # @return [Integer]
+    lazy_attr_reader(:count) {
+      node = at!('.layout-cell .count-badge')
+      node.inner_text.match(/\d+/).to_s.to_i
+    }
+    # @return [Boolean]
+    lazy_attr_reader(:last?) {
+      at!('li.pages-current').next_element.inner_text.to_i == 0
     }
     # @return [Array<Hash{Symbol=>Object}, nil>]
-    lazy_attr_reader(:illust_hashes) {
-      search!('.display_works li').map {|node| illust_hash_from_list_item(node) }
+    lazy_attr_reader(:page_hashes) {
+      search!('.display_works li').map {|n| hash_from_list_item(n) }
     }
+    # @return [Integer]
+    lazy_attr_reader(:member_id) {
+      doc.body.match(/pixiv\.context\.userId = '(\d+)'/).to_a[1].to_i
+    }
+
+    # @return [String]
+    def url
+      self.class.url(member_id, page)
+    end
+
+    # @return [Boolean]
+    def first?
+      page == 1
+    end
+
+    # @return [String]
+    def next_url
+      last? ? nil : self.class.url(member_id, page + 1)
+    end
+
+    # @return [String]
+    def prev_url
+      first? ? nil : self.class.url(member_id, page - 1)
+    end
 
     private
 
-    def illust_hash_from_list_item(node)
+    # @param [Nokogiri::XML::Node] node
+    # @return [Hash{Symbol=>Object}] illust_hash
+    def hash_from_list_item(node)
       return nil if node.at('img[src*="limit_unknown_s.png"]')
       illust_node = node.at('a')
       illust_id = illust_node['href'].match(/illust_id=(\d+)/).to_a[1].to_i
