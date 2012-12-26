19f6a758fcb4bfaefb90cc1454b9d2bfec307579
Tomoki Aonuma <uasi@uasi.jp>
2012-12-26 20:08:39 +0900

    Add new IllustList, the base class for illust list

---
+++ lib/pixiv/illust_list.rb
@@ -0,0 +1,28 @@
+module Pixiv
+  # @abstract
+  class IllustList < Page
+    include PageCollection
+
+    attr_reader :page
+    attr_reader :count
+
+    def page_class
+      Page
+    end
+
+    alias illust_hashes page_hash # TODO: pluralize PageCollection#page_hash
+    alias illust_urls page_urls
+  end
+
+  module IllustList::WithClient
+    include Page::WithClient
+    include Enumerable
+
+    def each
+      illust_hashes.each do |attrs|
+        url = attrs.delete(:url)
+        yield Illust.lazy_new(attrs) { client.agent.get(url) }
+      end
+    end
+  end
+end
