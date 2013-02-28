78dbe11cd38155b59449db9c760f4ec01d322e5a
Tomoki Aonuma <uasi@uasi.jp>
2013-03-01 00:00:35 +0900

    Fix doc: Member#pixiv_id returns a string

--- lib/pixiv/member.rb
+++ lib/pixiv/member.rb
@@ -14,7 +14,7 @@ module Pixiv
     lazy_attr_reader(:name) { at!('.profile-unit h1').inner_text }
     # @return [Integer]
     lazy_attr_reader(:member_id) { at!('link[hreflang=en]')['href'][%r{/users/(\d+)}, 1].to_i }
-    # @return [Integer]
+    # @return [String]
     lazy_attr_reader(:pixiv_id) { profile_image_url[%r{/profile/([a-z_-]+)/}, 1] }
     # @return [String]
     lazy_attr_reader(:profile_image_url) { at!('.profile-unit .user-image')['src'] }
